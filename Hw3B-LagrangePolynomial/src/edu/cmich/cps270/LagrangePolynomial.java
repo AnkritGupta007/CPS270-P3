d3c40ede560d0d16f3b891f881ac3c093776ef65
Ankrit Gupta <ankritgupta007@gmail.com>
2021-03-24 16:02:21 -0400

    completed the method

--- Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
+++ Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
@@ -9,14 +9,17 @@ interface Func {
 }
 
 public class LagrangePolynomial {
-	
-	public static void main(String[] args) {
 
-		
+	public static void main(String[] args) {
+		double[] xpoints = {0,0.6,0.9};
+		double[] fnAtxpoints = {1,0.8253,0.6216} ;
+		LagrangePolynomial pol = new LagrangePolynomial( xpoints, fnAtxpoints  ); 
+
+		System.out.println(pol.evaluateAt(0.45)); 
 	}
-	
+
 	List<Pair<Double, Double>> fnEvals;
-	
+
 	/**
 	 * 
 	 * @param xi an array of values at which the function is evaluated
@@ -24,13 +27,13 @@ public class LagrangePolynomial {
 	 */
 	public LagrangePolynomial(double[] xi, double[] fnAtxi) {
 		this.fnEvals = new ArrayList<Pair<Double, Double>>();
-		
+
 		for(int i = 0; i < xi.length; i++) {
 			fnEvals.add(new Pair<Double, Double>(xi[i], fnAtxi[i]));
 		}
-		
+
 	}
-	
+
 	/**
 	 * Evaluate a Lagrange interpolating polynomial at given x
 	 * 
@@ -38,14 +41,28 @@ public class LagrangePolynomial {
 	 * @return the value of the Lagrange polynomial evaluated at x
 	 */
 	public double evaluateAt(double x) {
-		
+
 		double sum = 0;
-		
+
 		// YOUR CODE HERE
-		
-		
+
+		Pair<Double, Double> currPair = null;
+		for (int n = 0; n < fnEvals.size(); n++) {
+			double kValue = fnEvals.get(n).getL();	
+
+			double Ln_k = 1.0;
+			for (int k = 0; k < fnEvals.size(); k++) {
+				if(k != n) {
+					currPair = fnEvals.get(k);
+					Ln_k = Ln_k * (x - currPair.getL()) / (kValue - currPair.getL());
+				}
+			}
+
+			sum = sum + fnEvals.get(n).getR() * Ln_k;
+		}
+
 		return sum;
 	}
-	
+
 }
 
