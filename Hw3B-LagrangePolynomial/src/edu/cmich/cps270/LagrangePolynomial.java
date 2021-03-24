df34020c297d3c57a69bdce4d2ab3b4072a9610d
Samuel St John <stjoh1sr@cmich.edu>
2021-03-24 15:44:40 -0400

    Adding question tests to main method and misc. commenting/JavaDoc

--- Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
+++ Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
@@ -1,5 +1,12 @@
 package edu.cmich.cps270;
 
+/**
+ * A class for implementing and testing a Lagrange interpolating polynomial.
+ * 
+ * @author stjoh1sr, gupta4a, black9m
+ * @since 24 MAR 2021
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +17,31 @@ interface Func {
 
 public class LagrangePolynomial {
 	
+	/**
+	 * Tests the implementation of the Lagrange polynomial.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
+		// Estimating the U.S. Population in 1975 and in 2020
+		double[] censusYear = {1950, 1960, 1970, 1980, 1990, 2000, 2010};
+		double[] censusPopulation = {151.3, 179.3, 203.3, 226.5, 249.6, 282.5, 309.3};
 		
+		LagrangePolynomial censusPolynomial = new LagrangePolynomial(censusYear, censusPopulation);
+		System.out.println("Census Estimate 1975: " + censusPolynomial.evaluateAt(1975));
+		System.out.println("Census Estimate 2020: " + censusPolynomial.evaluateAt(2020));
+		
+		// Estimating the Snipe population in 2017
+		double[] snipeYear = {2012, 2014, 2016, 2018, 2020};
+		double[] snipePopulation = {51, 1005, 20453, 405232, 1815700, 8137723};
+		
+		LagrangePolynomial snipePolynomial = new LagrangePolynomial(snipeYear, snipePopulation);
+		System.out.println("Snipe Pop. Estimate 2017: " + snipePolynomial.evaluateAt(2017));
 	}
 	
 	List<Pair<Double, Double>> fnEvals;
 	
 	/**
+	 * Initializes a Lagrange polynomial.
 	 * 
 	 * @param xi an array of values at which the function is evaluated
 	 * @param fnAtxi the evaluation of the function at the points in xi
@@ -40,7 +64,7 @@ public class LagrangePolynomial {
 	public double evaluateAt(double x) {
 		
 		double sum = 0;
-		
+		// TODO
 		// YOUR CODE HERE
 		
 		
