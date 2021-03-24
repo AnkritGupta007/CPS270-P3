0cf38d04be02b1850f7866260d5241c4eaf44da0
Ankrit Gupta <ankritgupta007@gmail.com>
2021-03-24 16:09:49 -0400

    edited the code

--- Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
+++ Hw3B-LagrangePolynomial/src/edu/cmich/cps270/LagrangePolynomial.java
@@ -15,7 +15,23 @@ public class LagrangePolynomial {
 		double[] fnAtxpoints = {1,0.8253,0.6216} ;
 		LagrangePolynomial pol = new LagrangePolynomial( xpoints, fnAtxpoints  ); 
 
-		System.out.println(pol.evaluateAt(0.45)); 
+		System.out.println(pol.evaluateAt(0.45));
+
+
+		// Estimating the U.S. Population in 1975 and in 2020
+		double[] censusYear = {1950, 1960, 1970, 1980, 1990, 2000, 2010};
+		double[] censusPopulation = {151.3, 179.3, 203.3, 226.5, 249.6, 282.5, 309.3};
+
+		LagrangePolynomial censusPolynomial = new LagrangePolynomial(censusYear, censusPopulation);
+		System.out.println("Census Estimate 1975: " + censusPolynomial.evaluateAt(1975));
+		System.out.println("Census Estimate 2020: " + censusPolynomial.evaluateAt(2020));
+
+		// Estimating the Snipe population in 2017
+		double[] snipeYear = {2012, 2014, 2016, 2018, 2020};
+		double[] snipePopulation = {51, 1005, 20453, 405232, 1815700, 8137723};
+
+		LagrangePolynomial snipePolynomial = new LagrangePolynomial(snipeYear, snipePopulation);
+		System.out.println("Snipe Pop. Estimate 2017: " + snipePolynomial.evaluateAt(2017));
 	}
 
 	List<Pair<Double, Double>> fnEvals;
